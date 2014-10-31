4bdb355f6f6a9e0701e2f8683f1abfd782dc72ba
scirelli <scirelli+git@gmail.com>
2014-10-30 21:56:25 -0400

    creating an api

--- js/aircraftclubs_popup.js
+++ js/aircraftclubs_popup.js
@@ -35,13 +35,94 @@ var client_id        = '920486903505-r6bkir5pv6td8iqh84rbu1k0gngv0s8n.apps.googl
  */
 document.getElementById('run').addEventListener('click',function(){
     var calendarlist = new gcal.CalendarList();
+
     calendarlist.list().then(function(aCalendarListEntry){
         console.log(aCalendarListEntry);
-    });
-    calendarlist.get('4aagckvcu89jtfn825l7tbdpi0@group.calendar.google.com').then(function(oCalenderListEntry){
+    }).done();
+
+    calendarlist.get('18s4kn3fhvriai8jujb1ss2m7k@group.calendar.google.com').then(function(oCalenderListEntry){
         console.log(oCalenderListEntry);
-    });
+    }).done();
+
+    calendarlist.insert({
+        id:'18s4kn3fhvriai8jujb1ss2m7k@group.calendar.google.com',
+        defaultReminders:[
+            {
+                method:'email',
+                minutes:10
+            }
+        ],
+        notificationSettings:{
+            notifications:[
+                {
+                    method:'email',
+                    type:'eventCreation'
+                }
+            ]
+        }
+    }).then(
+        function( obj ){
+        },
+        function( error ){
+            debugger;
+            console.error(error);
+        }
+    ).done();
+
+    calendarlist.delete('0vf8d7bhs203c0rk92as26v2qk@group.calendar.google.com').then(
+        function(oCalenderListEntry){
+            console.log('Delete: ');
+            debugger;
+        },
+        function(obj){
+            debugger;
+        }
+    ).done();
+
+    /*
+    calendarlist.insert({
+        id:'bgi5k124bf64bohkavm62qmu8o@group.calendar.google.com',
+        defaultReminders:[
+            {
+                method:'email',
+                minutes:10
+            }
+        ],
+        notificationSettings:{
+            notifications:[
+                {
+                    method:'email',
+                    type:'eventCreation'
+                }
+            ]
+        }
+    }).then(
+        function( obj ){
+        },
+        function( error ){
+            debugger;
+            console.error(error);
+        }
+    ).done();
+
+    calendarlist.get('bgi5k124bf64bohkavm62qmu8o@group.calendar.google.com').then(function(oCalenderListEntry){
+        console.log(oCalenderListEntry);
+    }).done();
+    */
+
+    //bgi5k124bf64bohkavm62qmu8o@group.calendar.google.com
+    /*
+    calendarlist.delete('18s4kn3fhvriai8jujb1ss2m7k@group.calendar.google.com').then(function(oCalenderListEntry){
+        console.log('Delete: ');
+        console.log(oCalenderListEntry);
+        debugger;
+    },
+    function(obj){
+        debugger;
+    }).done();
+    */
     calendarlist.execute();
+
     /*
     gcal.xhrWithAuth( 'GET', 'https://www.googleapis.com/calendar/v3/users/me/calendarList', true, null, function(r){
         debugger;
